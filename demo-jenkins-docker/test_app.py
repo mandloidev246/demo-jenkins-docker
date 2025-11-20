364586a0ee9cb315d9cf3bed1a570b1d7c3efc92
Dev_Mandloi <117761386+mandloidev246@users.noreply.github.com>
2025-11-21 00:00:04 +0530

    Delete demo-jenkins-docker directory
    delete dir

--- demo-jenkins-docker/test_app.py
+++
@@ -1,9 +0,0 @@
-import requests
-def test_root_response():
-    # start app separately during CI or use client in more advanced tests;
-    # here we do a simple smoke test using Flask's test client instead:
-    from app import app
-    client = app.test_client()
-    rv = client.get('/')
-    assert rv.status_code == 200
-    assert b"Hello from Jenkins + Docker CI/CD!" in rv.data
